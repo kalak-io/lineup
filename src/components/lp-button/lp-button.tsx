203183783ab5ff45d54f8714d4ea8adbe615849f
Clément Jacquemaire <clement@unifai.fr>
2024-03-11 09:34:28 +0100

    REMOVE useless component

--- src/components/lp-button/lp-button.tsx
+++ src/components/lp-button/lp-button.tsx
@@ -1,23 +1,26 @@
-import { Component, h } from "@stencil/core";
+import { Component, Prop, h } from '@stencil/core';
 
 /**
  * @slot default - Slot for the content of the button
  */
 
 @Component({
-  tag: "lp-button",
-  scoped: true,
+  tag: 'lp-button',
 })
 export class LpButton {
+  @Prop() callback?: Function;
+
   private handleClick = () => {
-    console.log("Received the button click!");
+    if (this.callback) {
+      this.callback();
+    }
   };
 
   render() {
     return (
       <div>
         <button onClick={this.handleClick}>
-          <slot>Button</slot>
+          <slot>Click on</slot>
         </button>
       </div>
     );
