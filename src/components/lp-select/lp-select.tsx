acfcdd329b53ab9ff636b4d927edfd9a9576009d
Clément Jacquemaire <clement.jacquemaire@gmail.com>
2024-03-23 23:05:28 +0100

    REMOVE useless code

--- src/components/lp-select/lp-select.tsx
+++ src/components/lp-select/lp-select.tsx
@@ -17,21 +17,16 @@ export class LpSelect {
   value: string = "";
 
   private handleChange = (event) => {
-    console.log('handle change');
-    console.log(event);
     const index = event.target.options.selectedIndex - 1 >= 0 ? event.target.options.selectedIndex - 1 : 0;
-
     this.value = this.options[index].name
-    console.log(this.value)
   }
-
 
   render() {
     return (
       <div>
         <select onChange={this.handleChange}>
             <option value=""><slot name="label">Select an option</slot></option>
-              {this.options.map(({ name, label }, index) => {
+              {this.options.map(({ name, label }) => {
                 return (
                   <option key={name} value={name}>{label}</option>
                   )
